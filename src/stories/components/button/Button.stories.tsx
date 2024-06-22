44e06e5b6952614fa652b9aa7d7d9f81a34c00e2
Minja Gaso <admin@minjagaso.com>
2024-06-22 16:18:22 -0500

    feat: add autodocs and create dynamic button toggling

--- src/stories/components/button/Button.stories.tsx
+++ src/stories/components/button/Button.stories.tsx
@@ -3,64 +3,54 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from "@/components/ui/button"
 
 const meta: Meta<typeof Button> = {
-  component: Button,
-  tags: ['autodocs'],
   title: 'Components/Button',
+  component: Button,
   argTypes: {
-        variant: {
-            control: 'select',
-            options: ['primary', 'secondary', 'destructive', 'outline', 'ghost', 'link' ] // Define your variants here
-        }
-  }
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'destructive'],
+    },
+    label: {
+      control: 'text',
+      description: 'Content to display on the button',
+    },
+  },
+  args: {
+    variant: 'primary',
+    label: 'Button',
+  },
+  render: (args) => {
+    const { variant } = args
+    if ( variant === 'primary' ) {
+      return <Button>{args.label}</Button>
+    }
+
+    return <Button {...args}>{args.label}</Button>
+  },
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
+
 export const Primary: Story = {
     args: {
-        label: 'Button',
         variant: 'primary',
-    },
-    render: (args) => <Button>{args.label}</Button>
+    }
 };
 
 export const Secondary: Story = {
     args: {
-      label: 'Button',
       variant: 'secondary',
-    },
-    render: (args) => <Button {...args}>{args.label}</Button>
+    }
 };
 
 export const Destructive: Story = {
     args: {
-      label: 'Button',
       variant: 'destructive',
-    },
-    render: (args) => <Button {...args}>{args.label}</Button>
+    }
 };
-
-// export const Secondary: Story = {
-//     args: {
-//       label: 'Button',
-//       variant: 'primary',
-//     },
-//     render: (args) => {
-//       const { variant } = args
-//       if ( variant === 'primary' ) {
-//           return <Button>{args.label}</Button>
-//       }
-//       if ( variant === 'icon' ) {
-//           return <Button variant="outline" size="icon">
-//               "icon goes here"
-//           </Button>
-//       }
-//       if ( variant === 'asChild' ) {
-//           return <Button asChild>
-//               <a href="/login">Login</a>
-//           </Button>
-//       }
-//       return <Button {...args}>{args.label}</Button>
-//     }
-//   };
