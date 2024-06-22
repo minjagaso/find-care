513e6a466267dc67d0c385d1e3150bb98119a780
Minja Gaso <admin@minjagaso.com>
2024-06-22 08:38:05 -0500

    feat: add queryparams context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryParamContext } from './contexts/QueryParamContext'
 
 function App() {
   const [count, setCount] = useState(0)
+  const queryParams = useContext(QueryParamContext);
+
+  console.log(queryParams)
 
   return (
     <>
